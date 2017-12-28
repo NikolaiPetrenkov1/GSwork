3c1ebf07d0b48a2c12ce675895088449565f063a
Петренков Николай <@corp.itstep>
2017-12-28 10:42:23 +0600

    GS работа

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOfGS myNewObject = new ClassOfGS();
+            int i = GC.GetGeneration(myNewObject);
+            Console.WriteLine("Current generation of object is {0}", i);
+            GC.Collect();
+            i = GC.GetGeneration(myNewObject);
+            Console.WriteLine("Current generation of object is {0}", i);
+            GC.Collect();
+            i = GC.GetGeneration(myNewObject);
+            Console.WriteLine("Current generation of object is {0}", i);
+            Console.Read();
+        }
+    }
+}
